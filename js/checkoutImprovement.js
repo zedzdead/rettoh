5f35ab9eb8177d895ca4322467ba0e103028dced
Andy Davies <andycarldavies@gmail.com>
2017-04-27 14:08:49 +0100

    Choose address message toggle class
    Toggle class for hide display purposes

--- js/checkoutImprovement.js
+++ js/checkoutImprovement.js
@@ -2,6 +2,8 @@
 /* Created 27/4/2017            */
 /* Author: Andy Davies          */
 /* Copyright: Hotter Shoes      */
+
+/*WS-2206 Click and collect Store Locator radio buttons*/
 jQuery(document).ready(function($) {
 	$('tr').click(function() {
 	  $(this).find('input:radio').prop('checked', true);
@@ -9,4 +11,12 @@ jQuery(document).ready(function($) {
 	  $(this).addClass('collect');
 	  $(this).find('td:nth-child(2)').append($('#WC_CheckoutStoreSelection_div_8').fadeIn('slow'));  
 	});
+});
+
+jQuery(document).ready(function($) {
+	if($('#shipping_delivery_method_option').length) {
+		$('#shipping_delivery_method_option').click(function() {
+			$('#WC_SingleShipmentShippingMethodDetails_div_1 p').toggleClass('addresstoggle');
+		});
+	}
 });
