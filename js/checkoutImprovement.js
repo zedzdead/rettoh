618590efefb42dd40afb4d5582977840c2eda216
Andy Davies <andycarldavies@gmail.com>
2017-04-27 14:41:50 +0100

    Remove alerts

--- js/checkoutImprovement.js
+++ js/checkoutImprovement.js
@@ -13,16 +13,14 @@ jQuery(document).ready(function($) {
 	});
 });
 
+/*WS-2237 Address Finder - "Please enter an Address" persists after use*/
 jQuery(document).ready(function($) {
 	if($('.shipping_method_item input').length) {
 		$('.shipping_method_item input').change(function() {
-			alert('clicked!');
 			$('#WC_SingleShipmentShippingMethodDetails_div_1 p').toggleClass('msghide');
 		});
 		$('#shipping_delivery_method_change').click(function() {
-			alert('clicked again!');
 			$('#WC_SingleShipmentShippingMethodDetails_div_1 p').toggleClass('msghide');
 		});
-
 	}
 });
