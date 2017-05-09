7c98990f8433c442558f0c6cc3ce1f4a604bc1fa
Andy Davies <andycarldavies@gmail.com>
2017-05-09 14:12:29 +0100

    dISABLE FOR NOW

--- js/formFields.js
+++ js/formFields.js
@@ -1,11 +1,11 @@
+/*
 jQuery(document).ready(function($) {
   console.log('Start Form Fields');
   if ($('input').length) {
     $('input').each(function() {
       //var phText = $(this).prev('label').text();
-      var phText =$(this).prev('label').contents().get(0).nodeValue;
+      var phText = $(this).prev('label').contents().get(0).nodeValue;
       console.log(phText);
-      /*
       $(this).prop('placeholder', phTest);   
       $(this).focus(function() {
         $(this).prev('label').addClass('placeholder');
@@ -15,8 +15,8 @@ jQuery(document).ready(function($) {
           $(this).prev('label').removeClass('placeholder');
         }
       });
-      */
     });
   }
   console.log('End Form Fields');
 });
+*/
