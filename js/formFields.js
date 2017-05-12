d485ef7ebb3dbeb71a85b4ddf699fa64c23cc96d
Andy Davies <andycarldavies@gmail.com>
2017-05-12 13:21:16 +0100

    Put everything back

--- js/formFields.js
+++ js/formFields.js
@@ -1,3 +1,22 @@
 jQuery(document).ready(function($) {
-
+  /*
+  console.log('Start Form Fields');
+  if ($('input').length) {
+    $('input').each(function() {
+      //var phText = $(this).prev('label').text();
+      var phText = $(this).prev('label').contents().get(0).nodeValue;
+      console.log(phText);
+      $(this).prop('placeholder', phTest);   
+      $(this).focus(function() {
+        $(this).prev('label').addClass('placeholder');
+      });
+      $('input').blur(function() {
+        if($(this).val().length == 0) {
+          $(this).prev('label').removeClass('placeholder');
+        }
+      });
+    });
+  }
+  console.log('End Form Fields');
+  */
 });
