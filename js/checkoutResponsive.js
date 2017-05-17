b157e46ae477da5bcf6dae5cde07278f502657f9
Andy Davies <andycarldavies@gmail.com>
2017-05-17 15:24:45 +0100

    Refactor that las change

--- js/checkoutResponsive.js
+++ js/checkoutResponsive.js
@@ -1,10 +1,11 @@
 jQuery(document).ready(function($) {
-	console.log('Looking for crumbs');
+	//Look for Checkout crumbs and set dev bar
 	if($('#checkout_crumb').length) {
 		console.log('This is Checkout');
 		$('body').addClass('checkout');
 		$('body').prepend($('<div id="dev"></dev>'));
 	}
+	//Add non breaking spaces to shipping option label text
 	if($('.shipping_method_item_title').length) {
 		$('.shipping_method_item_title label span').each(function() {
 			$(this).text($(this).text().replace(' ', '&nbsp;'));
