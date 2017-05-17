5e22803e25d02c61c01f2fec54b98c58428dbe70
Andy Davies <andycarldavies@gmail.com>
2017-05-17 14:10:18 +0100

    Stuff

--- js/checkoutResponsive.js
+++ js/checkoutResponsive.js
@@ -1,5 +1,6 @@
 jQuery(document).ready(function($) {
 	if($('#checkout_crumb').length) {
+		console.log('This is Checkout');
 		$('body').addClass('checkout');
 		$('body').prepend($('<div id="dev"></dev>'));
 	}
