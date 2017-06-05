9796d7f988fdf14e9da2af9d117bf9ced8052357
Andy Davies <andycarldavies@gmail.com>
2017-06-05 12:27:19 +0100

    Everything upto date

--- css/checkoutImprovement.js
+++ css/checkoutImprovement.js
@@ -3,7 +3,8 @@
 /* Author: Andy Davies          */
 /* Copyright: Hotter Shoes      */
 
-/*WS-2206 Click and collect Store Locator radio buttons*/
+//Replaced below
+/*
 jQuery(document).ready(function($) {
 	if($('#bopis_table').length) {
 		$('tr').click(function() {
@@ -13,6 +14,30 @@ jQuery(document).ready(function($) {
 		  $(this).find('td:nth-child(2)').append($('#WC_CheckoutStoreSelection_div_8').fadeIn('slow'));  
 		});
 	}
+});
+*/
+/*WS-2206 Click and collect Store Locator radio buttons*/
+jQuery(document).ready(function($) {
+	console.log('Start Click and collect Labels');
+	if($('#bopis_table').length) {
+		//Add a label element for each radio button
+		$('#bopis_table input[type="radio"]').each(function() {
+			var name   = $(this).attr('name');
+			var ps_val = $(this).val();
+
+			$(this).attr('id', name + '-' + ps_val);
+			$(this).before($('<label class="full-line" for="' + name + '-' + ps_val +'">Branch</label>'));
+		});
+		//Find the selected branch and contextually add the continue button 
+		$('#bopis_table input[type="radio"]').change(function() {
+		//$('#bopis_table label').click(function() {	
+			//$('#WC_CheckoutStoreSelection_div_8').hide();
+			$('tr').removeClass('selected');
+			$(this).parentsUntil('tbody').addClass('selected');
+			$('.selected td:nth-child(2)').append($('#WC_CheckoutStoreSelection_div_8'));
+		});
+	}
+	console.log('End Click and Collect Labels');
 });
 
 /*WS-2237 Address Finder - "Please enter an Address" persists after use*/
