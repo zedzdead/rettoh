918898b4561ea2d160d7be77926b8910fdf5fec6
Andy Davies <andycarldavies@gmail.com>
2017-07-14 16:34:49 +0100

    Add old footer code

--- js/checkoutImprovement.js
+++ js/checkoutImprovement.js
@@ -17,6 +17,51 @@ jQuery(document).ready(function($) {
 	}
 });
 */
+//Old Mobile Secure Footer Functions
+jQuery(document).ready(function($) {
+	if (jQuery('#WC_SingleShipmentShippingMethodDetails_div_1').length > 0)
+	{
+		jQuery('#WC_SingleShipmentShippingMethodDetails_div_1 > p:eq(0)').html('Please select one of the delivery options below')
+	}
+
+	//on reset options remove red banner
+	jQuery(document).on('click', '#shipping_delivery_method_change', function () {
+		jQuery("#SelecDelBan").css("display", "none");
+	});
+
+	//move red box if screen resized
+	jQuery(window).resize(function() {
+	if( jQuery("#shipping_delivery_method_option_desc").length > 0 ) {
+	  var banposofftop2 = jQuery("#shipping_delivery_method_option_desc").offset().top + 30;
+		var banposoffleft2 = jQuery("#shipping_delivery_method_option_desc").offset().left - 26;
+		jQuery("#SelecDelBan").css({"top":banposofftop2+ "px","left":banposoffleft2+ "px","position": "absolute" });
+	}
+	});
+
+	//Add time to click and collect message
+	jQuery("label:contains('Collect in store')").html('Collect in store <span>Allow up to 7 days</span>')
+
+	//change time of standard delivery
+	jQuery("span:contains('Allow up to 5 Working days')").html("Allow 3-5 Working days");
+
+	//remove shipping methods for SS
+	if (jQuery(".address:contains('WN8 9PT')").length > 0)
+	{
+		jQuery('.shipping_method_item').eq(1).css('display','none')
+		jQuery('.shipping_method_item').eq(2).css('display','none')
+	}  
+
+	jQuery('body').on('click', '#JSShippingId_10651', function () {
+		//Set Red banner
+		var banposofftop = jQuery("#shipping_delivery_method_option_desc").offset().top + 30;
+		var banposoffleft = jQuery("#shipping_delivery_method_option_desc").offset().left - 26;
+		jQuery("#SelecDelBan").css("display", "block");
+		jQuery("#SelecDelBan").css({"top":banposofftop+ "px","left":banposoffleft+ "px","position": "absolute" });
+	});
+
+});
+
+
 /*WS-2206 Click and collect Store Locator radio buttons*/
 jQuery(document).ready(function($) {
 	console.log('Start Click and collect Labels');
